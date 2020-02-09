378ff89ded159a137ce4493bca2b4bae0bc6f1da
demonlibra <demonlibra@inbox.ru>
2020-02-09 09:34:26 +0300

    BLTOUCH/LERDGE - Some fixes

--- bltouch/bltouch.sh
+++ bltouch/bltouch.sh
@@ -7,7 +7,7 @@ MAX_X=230							#Максимальная координата X
 MAX_Y=180							#Максимальная координата Y
 
 POINTS_X=10							#Точек измерения вдоль оси X
-POINTS_Y=10							#Точек измерения вдоль оси Y
+POINTS_Y=7							#Точек измерения вдоль оси Y
 
 file_data="bltouch_lerdge_output"	#Файл вывода данных от Lerdge
 file_mesh="bltouch_mesh"			#Файл сетки координат
@@ -18,14 +18,14 @@ stty 115200 -F /dev/ttyUSB0 raw -echo
 sleep 3
 cat /dev/ttyUSB0 > "$file_data" &
 
-STEP_X=$(( ($MAX_X-$MIN_X) / $POINTS_X ))
-STEP_Y=$(( ($MAX_Y-$MIN_Y) / $POINTS_Y ))
+STEP_X=$(( ($MAX_X-$MIN_X) / ($POINTS_X-1) ))
+STEP_Y=$(( ($MAX_Y-$MIN_Y) / ($POINTS_Y-1) ))
 
 Y=$MIN_Y
-while [[ $Y -lt $MAX_Y ]]
+while [[ $Y -le $MAX_Y ]]
 	do
 		X=$MIN_X
-		while [[ $X -lt $MAX_X ]]
+		while [[ $X -le $MAX_X ]]
 			do
 				echo "G30 X$X Y$Y P1" > /dev/ttyUSB0
 				X=$(( $X + $STEP_X ))
