db15b00996c285a1f7a72a793f4b9a712c3fa1a8
demonlibra <demonlibra@inbox.ru>
2020-02-19 21:51:06 +0300

    BLTOUCH/LERDGE - Some fixes

--- bltouch/README.md
+++ bltouch/README.md
@@ -2,7 +2,6 @@
 # Mesure surface of table with BL-TOUCH/3D-TOUCH and Lerdge
 
 ## Почему так сделано?
-
 После выполнения процедуры измерения стола (G29), Lerdge не выводит результаты в терминал.
 
 ## Как это работает?
@@ -17,7 +16,7 @@
 | `bltouch.sh` | Основной сценарий bash. Отправляет команды через UART, собирает и обрабатывает данные.|
 | `bltouch_lerdge_output` | Вывод данных с UART. Преобразуется в файл `bltouch_mesh`. |
 | `bltouch_mesh` | Массив точек. Подготовленный для обработки в `gnuplot`. |
-| `bltouch_plot` | Сценарий для gnuplot. Формирует изображение (в формате png) поверхности из массива точек `bltouch_mesh`. |
+| `bltouch_gnuplot` | Сценарий для gnuplot. Формирует изображение (в формате png) поверхности из массива точек `bltouch_mesh`. |
 
 ## Подготовка
 ## Prepare
@@ -32,15 +31,15 @@ For installation `gnuplot` run in terminal:
 
 ### Размещение файлов
 ### Place files
-- Create folder `\home\username\bltouch`.
-- Copy files `bltouch.sh` and `bltouch_plot`.
+- Create folder `\home\username\bltouch`.   
+- Copy files `bltouch.sh` and `bltouch_plot`.   
 
-- Создайте каталог bltouch в домашнем каталоге.
+- Создайте каталог bltouch в домашнем каталоге.   
 - Скопируйте в каталог bltouch файлы `bltouch.sh` и `bltouch_plot`.
 
 ### Параметры
 ### Parameters
-File `bltouch.sh` contain main parameters at the top.
+File `bltouch.sh` contain main parameters at the top.   
 В начале файла `bltouch.sh` задаются основные параметры:
 - зона измерений
 - количество точек
@@ -48,10 +47,15 @@ File `bltouch.sh` contain main parameters at the top.
 
 ## Запуск
 ## Run
-Open terminal in folder `\home\username\bltouch` and run command `bash bltouch.sh`.
+Open terminal in folder `\home\username\bltouch` and run command `bash bltouch.sh`.  
 Для запуск выполните в терминале `bash bltouch.sh`.
 
-After finish mesure procedure You will get image `bltouch_plot.png`.
-В результате будет создан файл `bltouch_plot.png` с изображением поверхности.
+After finish mesure procedure You will get image `bltouch_plot.png`.  
+В результате будет создан файл `bltouch_plot.png` с изображением поверхности.  
 
 ![bltouch_plot.png](https://github.com/demonlibra/uni/blob/master/bltouch/bltouch_plot.png)
+
+## Примечания 
+## Notes
+Отключите все сторонние подключения к плате принтера, такие OctoPrint, Repetier, ...  
+Disconnect all connectons to printer, for example OctoPrint, Repetier, ...  
--- bltouch/bltouch.sh
+++ bltouch/bltouch.sh
@@ -1,25 +1,37 @@
 #------------------- Параметры -------------------------------------------------------
 
-MIN_X=20							#Минимальная координата X
-MIN_Y=20							#Минимальная координата Y
+MIN_X=20				#Минимальная координата X
+MIN_Y=20				#Минимальная координата Y
 
-MAX_X=230							#Максимальная координата X
-MAX_Y=180							#Максимальная координата Y
+MAX_X=230				#Максимальная координата X
+MAX_Y=170				#Максимальная координата Y
 
-POINTS_X=10							#Точек измерения вдоль оси X
-POINTS_Y=7							#Точек измерения вдоль оси Y
+POINTS_X=10				#Точек измерения вдоль оси X
+POINTS_Y=7				#Точек измерения вдоль оси Y
 
 file_data="bltouch_lerdge_output"	#Файл вывода данных от Lerdge
-file_mesh="bltouch_mesh"			#Файл сетки координат
-file_plot="bltouch_plot"			#Сценарий для программы построения поверхности
-file_png="bltouch_plot.png"			#Файл вывода изображения поверхности
+file_mesh="bltouch_mesh"		#Файл сетки координат
+gnuplot_script="bltouch_gnuplot"	#Сценарий для программы построения поверхности
+file_png="bltouch_plot"			#Файл вывода изображения поверхности
 #-------------------------------------------------------------------------------------
+
+time_stamp=$(date +%F-%H-%M)
+
 stty 115200 -F /dev/ttyUSB0 raw -echo
 sleep 3
-cat /dev/ttyUSB0 > "$file_data" &
 
-STEP_X=$(( ($MAX_X-$MIN_X) / ($POINTS_X-1) ))
-STEP_Y=$(( ($MAX_Y-$MIN_Y) / ($POINTS_Y-1) ))
+cat /dev/ttyUSB0 > "$file_data"_"$time_stamp" &
+echo "G28" > /dev/ttyUSB0
+
+if [[ $POINTS_X -gt 1 ]]
+	then STEP_X=$(( ($MAX_X-$MIN_X) / ($POINTS_X-1) ))
+	else STEP_X=$MAX_X
+fi
+
+if [[ $POINTS_Y -gt 1 ]]
+	then STEP_Y=$(( ($MAX_Y-$MIN_Y) / ($POINTS_Y-1) ))
+	else STEP_Y=$MAX_Y
+fi
 
 Y=$MIN_Y
 while [[ $Y -le $MAX_Y ]]
@@ -36,21 +48,22 @@ while [[ $Y -le $MAX_Y ]]
 echo ; echo ------------------ ; read -p "Нажмите ENTER после завершения измерений"
 killall -q cat
 
-echo "#BL-TOUCH MESH" > "$file_mesh"
+echo "#BL-TOUCH MESH" > "$file_mesh"_"$time_stamp"
 
 i=1
-cat "$file_data" | while read line
+cat "$file_data"_"$time_stamp" | while read line
 do
 	if [[ `echo $line | grep -c "Starts probing"` = 0 ]] && [[ `echo $line | grep -c "position of the Z-axis"` = 1 ]]
 		then
 			line=`echo $line | sed s/"Z-axis"//g`
 			line=`echo $line | sed s/[^0-9.:,-]//g`
 			line=`echo $line | sed 's/\(:\|\,\)/ /g'`
-			echo $line >> "$file_mesh"
+			echo $line >> "$file_mesh"_"$time_stamp"
 			
 			i=$(( $i + 1 ))
-			if [[ $i = $(($POINTS_X + 1)) ]]; then i=1; echo "" >> "$file_mesh"; fi
+			if [[ $i = $(($POINTS_X + 1)) ]]; then i=1; echo "" >> "$file_mesh"_"$time_stamp"; fi
 		fi
 done
-
-gnuplot -e "file_mesh=\"$HOME/bltouch/$file_mesh\"" -e "file_out=\"$HOME/bltouch/$file_png\"" bltouch_plot
+path1="$HOME/bltouch/$file_mesh"_"$time_stamp"
+path2="$HOME/bltouch/$file_png"_"$time_stamp".png
+gnuplot -e "file_mesh=\"$path1\"" -e "file_out=\"$path2\"" "$gnuplot_script"
