eb9550836a4d44944559e6ae9bc17bf237144e54
demonlibra <demonlibra@inbox.ru>
2022-06-18 18:57:13 +0300

    separate project

--- power/scripts/poweroff.sh
+++
@@ -1,4 +0,0 @@
-echo "M81" > /dev/ttyUSB0
-sudo service octoprint stop
-sudo python /home/pi/neo_off.py
-sudo poweroff -p
--- power/scripts/restart.sh
+++
@@ -1,4 +0,0 @@
-echo "M999" > /dev/ttyUSB0
-sudo service octoprint stop
-sudo python /home/pi/neo_off.py
-sudo reboot
